69a7fb697547ca17f2ef6b4b7c005bdf2cf5cad7
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-07-12 17:48:01 -0400

    feat: add functionality to mobile menu
    - Add animation class js-mobile-menu-hidden
    - Add css class js-mobile-menu-hidden in header-container__menu-container
    - Add animation class js-mobile-menu-shown
    - Add animation classes in main.js file to animate mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
+// DOM ELEMENTS:
+const mobileMenuContainer = document.querySelector('.header-container__menu-container');
+const mobileMenuButton = document.querySelector('.menu-icon');
+const closeMenuButton = document.querySelector('.close-mobile-menu');
+
+// EVENT LISTENERS:
+mobileMenuButton.addEventListener('click',openMobileMenu);
+closeMenuButton.addEventListener('click', closeMobileMenu);
 
 
+
+function openMobileMenu() {
+ mobileMenuContainer.classList.add('js-mobile-menu-shown');
+ mobileMenuContainer.classList.remove('js-mobile-menu-hidden');
+};
+
+function closeMobileMenu() {
+  mobileMenuContainer.classList.remove('js-mobile-menu-shown');
+  mobileMenuContainer.classList.add('js-mobile-menu-hidden');
+};
 
